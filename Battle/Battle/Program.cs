2ce896369eb11086ba838e3e2fc8795c101f4557
Sefton Munro <30001654@student.toiohomai.ac.nz>
2018-08-01 08:57:45 +1200

    1

---
+++ Battle/Battle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu;
+            do
+            {
+                Console.WriteLine("");
+                menu = Console.ReadLine();
+                switch (menu)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "e":
+                        break;
+                    default:
+                        Console.WriteLine("Error invalid input!");
+                        break;
+                }
+            } while (menu != "e");
+        }
+    }
+}
